6fca76a489f15342ca4d0d37fb9a02483e739a6d
Anna <anna.chlusewicz@gmail.com>
2022-07-09 16:45:17 +0200

    post_list view, post_detail view added and a little of css

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,10 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publish')
+    list_display = ('title', 'slug', 'author', 'publish')
     list_filter = ('created', 'publish')
     search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('-publish',)
