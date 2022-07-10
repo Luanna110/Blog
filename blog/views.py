27d171c67602eb2760b3538e26d4315a3afdc4b0
Anna <anna.chlusewicz@gmail.com>
2022-07-10 15:05:00 +0200

    pagination added

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,18 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.objects.all()
+    paginator = Paginator(object_list, 4)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, publish__year=year, publish__month=month, publish__day=day)
